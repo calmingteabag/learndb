dd7728756855a37d3c079384913d2b4a4c1bd14c
badcompwaifu <phasedkamo@gmail.com>
2022-05-03 03:13:43 -0300

    Learned and solved path issues

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import route_a from './routes/routes.js'
 import express from 'express'
+
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// const fullpath = fileURLToPath(import.meta.url)
+// console.log(`Full path to file: ${fullpath}`)
+// const pathfile = path.dirname(fullpath)
+// console.log(`Path to File: ${pathfile}`)
+
+// console.log(import.meta.url)
+
 const app = express()
+const port = 8000
 
 app.use(route_a)
 
-app.listen(3000, () => {
-    console.log("app running")
+app.listen(port, () => {
+    console.log(`App running on port: ${port}`)
 })
