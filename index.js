50d50d011ce73c621b8e6e8b31dae58918dda889
badcompwaifu <phasedkamo@gmail.com>
2022-05-03 03:17:59 -0300

    Cleaned file

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 import route_a from './routes/routes.js'
 import express from 'express'
 
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-// const fullpath = fileURLToPath(import.meta.url)
-// console.log(`Full path to file: ${fullpath}`)
-// const pathfile = path.dirname(fullpath)
-// console.log(`Path to File: ${pathfile}`)
-
-// console.log(import.meta.url)
-
 const app = express()
 const port = 8000
 
