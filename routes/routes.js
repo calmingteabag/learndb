2bc3220a41a839b3d98720edc838a4eeb65d7522
badcompwaifu <phasedkamo@gmail.com>
2022-05-03 03:16:02 -0300

    path now works with ES6

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,21 @@
+import express from 'express'
 import { Router } from 'express'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+console.log(__dirname)
 const router = Router()
 
+router.use(express.static(path.join(__dirname, '../', '/static')))
+
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname + '/templates/index.html'))
+    res.sendFile(path.join(__dirname, '../static/html/index.html'));
 })
 
-router.get('/test', (req, res, next) => {
-    res.send('test page from routes.js')
+router.get('/waifu', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../static/img/waifu.jpg'))
 })
 
 export default router
