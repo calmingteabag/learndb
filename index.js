a764f002c14e4d543d976a8d7f4db8c1f775716b
badcompwaifu <phasedkamo@gmail.com>
2022-05-04 02:26:58 -0300

    Setup basic POST req to refresh how did it work

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 8000
 
 app.set('view engine', 'ejs')
 app.use(routes)
+// app.use(express.urlencoded({ extended: true }))
 
 app.listen(port, () => {
     console.log(`App running on port: ${port}`)
--- routes/routes.js
+++ routes/routes.js
@@ -5,10 +5,10 @@ import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-console.log(__dirname)
 const router = Router()
 
 router.use(express.static(path.join(__dirname, '../', '/static')))
+router.use(express.urlencoded({ extended: true }))
 
 router.get('/', (req, res, next) => {
     // res.sendFile(path.join(__dirname, '../static/html/index.html'));
@@ -18,5 +18,28 @@ router.get('/', (req, res, next) => {
 router.get('/waifu', (req, res, next) => {
     res.sendFile(path.join(__dirname, '../static/img/waifu.jpg'))
 })
+
+router.post('/test', (req, res) => {
+    let data = req.body.testform
+    res.render(path.join(__dirname, '../', '/static/html/index'), { waifu: "maiWaifu" })
+})
+/* 
+In python, post requests ar handled by view functions using 'if method=post'
+
+Here what we'll do is handle it with app.post.
+HTML method should be POST (for obvious reasons) and action should point to
+desired app.post endpoint.
+
+example:
+<form method="POST" action="/learned_stuff">
+  <input type="text" name="learned_entries"/>
+  <input type="submit" />
+</form>
+
+app.post('/learned_stuff, (req, res) => {
+    const learnedStuff = req.body.learned_entries
+    // process data, add to databases, etc
+})
+*/
 
 export default router
