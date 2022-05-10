5df892fca38547efcb371077eefca37a8ce32d99
badcompwaifu <phasedkamo@gmail.com>
2022-05-10 10:23:11 -0300

    deleted files

--- static/js_front/delete_form_handler.js
+++
@@ -1,9 +0,0 @@
-
-const getCheckBoxValue = () => {
-    let checkboxElems = document.getElementsByClassName("checkbox")
-    for (let elem of checkboxElems) {
-        console.log(elem.checked)
-    }
-
-}
-document.addEventListener('DOMContentLoaded', () => { getCheckBoxValue() }, false)
