64a7832c7d2d09f6461acd571a428f2183a7adeb
badcompwaifu <phasedkamo@gmail.com>
2022-05-24 05:40:53 -0300

    fixing static files

--- db/db_conn.js
+++ db/db_conn.js
@@ -1,12 +1,19 @@
 import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-});
+const dbDialect = 'postgres'
+const dbAddress = process.env.DATABASE_URL
+const sequelize = new Sequelize(dbAddress, {
+    dialect: `${dbDialect}`,
+    logging: false
+})
+
+// sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//         }
+//     }
+// });
 
 export { sequelize }
