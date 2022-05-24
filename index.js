8ac1776deda26c36d734ff9ac689c2e9fd05928f
badcompwaifu <phasedkamo@gmail.com>
2022-05-24 05:47:59 -0300

    fixing static files

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ newRouter.use(express.static(path.join(__dirname + '/static')))
 newRouter.use(express.urlencoded({ extended: true }))
 
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
