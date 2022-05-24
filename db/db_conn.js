11ba7a6a396f6892f37da571d300c56f834edd0f
badcompwaifu <phasedkamo@gmail.com>
2022-05-24 03:41:26 -0300

    changed to heroku postgres

--- db/db_conn.js
+++ db/db_conn.js
@@ -1,11 +1,12 @@
 import { Sequelize } from 'sequelize'
 
-const dbDialect = 'postgres'
-const dbDatabaseName = 'learndb'
-const dbAddress = `${dbDialect}://postgres:admin@localhost:5432/${dbDatabaseName}`
-const sequelize = new Sequelize(dbAddress, {
-    dialect: `${dbDialect}`,
-    logging: false
-})
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
 
-export { sequelize, dbAddress, dbDialect, dbDatabaseName }
+export { sequelize }
