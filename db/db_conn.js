581525dea981857ebd6fceca999e2257c7b0f927
badcompwaifu <phasedkamo@gmail.com>
2022-05-30 15:26:04 -0300

    changed db for deploy

--- db/db_conn.js
+++ db/db_conn.js
@@ -2,21 +2,21 @@ const { Sequelize } = require('sequelize')
 const dbDialect = 'postgres'
 
 const dbAddress = process.env.DATABASE_URL
-// const sequelize = new Sequelize(dbAddress, {
-//     dialect: `${dbDialect}`,
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false
-
-//         }
-//     },
-//     logging: false
-// })
-
-const sequelize = new Sequelize(dbAddress, { // !!!!! DEVELOPMENT !!!!!!
+const sequelize = new Sequelize(dbAddress, {
     dialect: `${dbDialect}`,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+
+        }
+    },
     logging: false
 })
+
+// const sequelize = new Sequelize(dbAddress, { // !!!!! DEVELOPMENT !!!!!!
+//     dialect: `${dbDialect}`,
+//     logging: false
+// })
 
 module.exports = sequelize
